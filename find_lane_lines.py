37c60f4307f9eaf9d02c6911596229cc628a4c88
shawpan <shawpan.du@gmail.com>
2017-02-01 00:16:39 +0600

    Refactored

--- find_lane_lines.py
+++ find_lane_lines.py
@@ -30,19 +30,17 @@ def process_image(img):
 
     return processed_image
 
-def find_lane_lines():
-    """ Find lanes from images or video1
+def find_lane_lines(type):
+    """ Find lanes from images or video
+    Args:
+        type: type of action, image or video
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--type', help='type i/v')
-    args = parser.parse_args()
-
-    if args.type == 'v':
+    if type == 'v':
         clip = VideoFileClip("./project_video.mp4")
         output_video = "./output_video/project_video.mp4"
         output_clip = clip.fl_image(process_image)
         output_clip.write_videofile(output_video, audio=False)
-    elif args.type == 'i':
+    elif type == 'i':
         images = glob.glob('test_images/test*.jpg')
         for idx, fname in enumerate(images):
             print('Processing image ', idx)
@@ -54,19 +52,31 @@ def find_lane_lines():
     else:
         print('Invalid type requested')
 
-
-find_lane_lines()
-
 def doc():
+    """ Execute and generate debug images
+    """
     src = np.float32([[240,719],[579,450],[712,450],[1165,719]])
     dst =  np.float32([[300,719],[300,0],[900,0],[900,719]])
     transformer = PerspectiveTransformer(src, dst)
     image = cv2.imread('doc/distorted.jpg')
-    processed_image = undistort(image, objpoints, imgpoints)
-    processed_image = process_binary(processed_image)
-    # processed_image = transformer.transform(processed_image)
-    # print('wtf')
-    # cv2.imwrite('doc/birdeyeview.jpg', processed_image*255)
-    # print('wtf')
+    undistort_image = undistort(image, objpoints, imgpoints)
+    cv2.imwrite('doc/undistorted.jpg', undistort_image)
+    processed_image = process_binary(undistort_image)
+    cv2.imwrite('doc/binary.jpg', processed_image*255)
+    processed_image = transformer.transform(processed_image)
+    cv2.imwrite('doc/birdeyeview.jpg', processed_image*255)
+    left_fit, right_fit, yvals, out_img = find_lanes(processed_image)
+    cv2.imwrite('doc/lanes.jpg', out_img)
+    processed_image = fit_lane(processed_image, undistort_image, yvals, left_fit, right_fit, transformer)
+    left_curvature, right_curvature, distance = get_curvature(left_fit, right_fit, yvals)
+    processed_image = draw_stat(processed_image, left_curvature, right_curvature, distance)
+    cv2.imwrite('doc/final.jpg', processed_image)
 
-# doc()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--type', help='type i/v/d')
+    args = parser.parse_args()
+    if args.type == 'd':
+        doc()
+    else:
+        find_lane_lines(args.type)
