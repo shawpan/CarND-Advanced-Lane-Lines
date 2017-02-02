de93fa6754faebbc48a30319383a94fb9d4dde15
shawpan <shawpan.du@gmail.com>
2017-02-02 16:06:33 +0600

    Improvement: Adjust lane width in meter to px ratio
    US standard of lane width 3.7m is 590px in the image. Previously it was 700px which has been adjusted to 590px to make the distance calculation more accurate
    
    With this change measurement information is more accurate.

--- image_processing.py
+++ image_processing.py
@@ -181,7 +181,7 @@ def get_curvature(leftx, rightx, ploty):
     """
     # Define conversions in x and y from pixels space to meters
     ym_per_pix = 30/720 # meters per pixel in y dimension
-    xm_per_pix = 3.7/700 # meters per pixel in x dimension
+    xm_per_pix = 3.7/590 # meters per pixel in x dimension
     y_eval = np.max(ploty)
     # ploty = np.linspace(0, 719, num=720)
     # Fit new polynomials to x,y in world space
